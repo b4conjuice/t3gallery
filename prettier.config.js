d0c9139bcafa6469febf0e107e5dc9fd3bdf55b3
Daniel Lopez <dlopez807@gmail.com>
2024-04-14 20:35:27 -0700

    update pretter config

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,13 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-};
+  plugins: ['prettier-plugin-tailwindcss'],
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  semi: false,
+}
 
-export default config;
+export default config
