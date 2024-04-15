fb3168362992c3700f46f248175aac088c02892f
Daniel Lopez <dlopez807@gmail.com>
2024-04-15 16:23:54 -0700

    setup database

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - [x] make it deploy (vercel)
 - [x] scaffold basic ui with mock data
 - [x] tidy up build process
-- [ ] actually set up db (vercel postgres)
+- [x] actually set up db (vercel postgres)
 - [ ] attach db to ui
 - [ ] add auth (clerk)
 - [ ] add image upload
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from '@/server/db'
+
 const mockUrls = [
   'https://utfs.io/f/e461c612-f375-4d52-9ed4-c19636464cb9-o7zgr4.jpg',
   'https://utfs.io/f/813ec977-4e31-4211-966f-171517d64be9-1jnw0z.jpg',
@@ -10,10 +12,17 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }))
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany()
+  console.log(posts)
   return (
     <main className=''>
       <div className='flex flex-wrap gap-4'>
+        {posts.map(post => (
+          <div key={post.id} className='w-48'>
+            {post.name}
+          </div>
+        ))}
         {[...mockImages, ...mockImages, ...mockImages].map(image => (
           <div key={image.id} className='w-48'>
             <img src={image.url} className='' />
