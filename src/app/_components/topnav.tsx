4e69f3a96cf22265d4b2f0ae2d2c7b2630fa819e
Daniel Lopez <dlopez807@gmail.com>
2024-04-15 17:39:39 -0700

    add authentication

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,20 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+
+export default function TopNav() {
+  return (
+    <nav className='flex w-full items-center justify-between border-b border-cb-dusty-blue p-4'>
+      <div className='text-3xl font-extrabold leading-9 tracking-tight sm:text-4xl sm:leading-10'>
+        gallery
+      </div>
+
+      <div className='text-xl font-semibold'>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  )
+}
