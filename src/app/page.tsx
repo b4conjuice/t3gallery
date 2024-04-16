c6b98c4fb71761c5a4dc5ace2b753f529e3b7148
Daniel Lopez <dlopez807@gmail.com>
2024-04-16 16:15:56 -0700

    add next/image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { SignedIn, SignedOut } from '@clerk/nextjs'
 
 import { getMyImages } from '@/server/queries'
+import Image from 'next/image'
 
 export const dynamic = 'force-dynamic'
 
@@ -9,8 +10,14 @@ async function Images() {
   return (
     <div className='flex flex-wrap gap-4'>
       {images.map(image => (
-        <div key={image.id} className='flex w-48 flex-col'>
-          <img src={image.url} className='' />
+        <div key={image.id} className='flex h-48 w-48 flex-col'>
+          <Image
+            src={image.url}
+            style={{ objectFit: 'contain' }}
+            width={192}
+            height={192}
+            alt={image.name}
+          />
           <div>{image.name}</div>
         </div>
       ))}
